aef2d9bb6e853d0a6e85679079b64d78cb3d3d55
zhangshun <2213397918@qq.com>
2018-12-05 16:37:04 +0800

    完成了大神和老板登录和注册的信息完善保存后的地址跳转

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,16 +1,22 @@
 import React, {Component} from 'react';
-import {Route} from  'react-router-dom';
+import {Route,Redirect} from  'react-router-dom';
+import Cookies from  'js-cookie';
 import BossInfo from '../../containers/boss-info';
-import DaShenInfo from '../../containers/dashen-info'
+import DaShenInfo from '../../containers/dashen-info';
+
 class Main extends Component {
-  render () {
-    return (
-        <div>
-            <Route path='/bossinfo' component={BossInfo}/>
-            <Route path='/dasheninfo' component={DaShenInfo}/>
-        </div>
-    )
+  render  () {
+          const userid = Cookies.get('userid');
+          if (!userid) return <Redirect to="/login" />
+
+      return (
+          <div>
+              <Route path='/bossinfo'  component={BossInfo}/>
+              <Route path='/dasheninfo' component={DaShenInfo}/>
+          </div>
+      )
   }
 }
+
 
 export default Main;
